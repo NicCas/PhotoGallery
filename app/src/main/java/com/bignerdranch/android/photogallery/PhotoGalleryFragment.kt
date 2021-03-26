0812adc675d028ddc4840063eb91b2de6c42cb5f
Nicole C <nicas@sonic.net>
2021-03-25 20:08:03 -0700

    Editing SharedPreferences with Android KTX

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -103,6 +103,10 @@ class PhotoGalleryFragment : Fragment() {
                     return false
                 }
             })
+
+            setOnSearchClickListener {
+                searchView.setQuery(photoGalleryViewModel.searchTerm, false)
+            }
         }
     }
 
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -10,6 +10,10 @@ class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
 
+    val searchTerm: String
+        get() = mutableSearchTerm.value ?: ""
+
+
     init {
         mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
         galleryItemLiveData =
